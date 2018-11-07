a1260b37cb60e43120772e9db43e8bfcd6cf7061
kennedy <kennedy@fastmail.us>
2018-11-06 21:24:35 -0500

    initial github page

---
+++ docs/precache-manifest.631c31b10600e46af3061623b650e1bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c760080037f47b495d3c",
+    "url": "/static/js/main.c7600800.chunk.js"
+  },
+  {
+    "revision": "6bb25aad7cc73fa91b37",
+    "url": "/static/js/1.6bb25aad.chunk.js"
+  },
+  {
+    "revision": "c760080037f47b495d3c",
+    "url": "/static/css/main.bbaf466c.chunk.css"
+  },
+  {
+    "revision": "9df0b51e847651cb5f529174be7b51ad",
+    "url": "/index.html"
+  }
+];
