5bbddc534032dbb0fb40acfd4df8c65ba1843ffe
kennedy <kennedy@fastmail.us>
2018-11-06 21:38:09 -0500

    build

---
+++ docs/precache-manifest.a5c1d48ef70ea034d1be65d78e5b5210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9bd7d7cb6e1d8c420b2",
+    "url": "/junctions-io-web/static/js/runtime~main.f9bd7d7c.js"
+  },
+  {
+    "revision": "c760080037f47b495d3c",
+    "url": "/junctions-io-web/static/js/main.c7600800.chunk.js"
+  },
+  {
+    "revision": "6bb25aad7cc73fa91b37",
+    "url": "/junctions-io-web/static/js/1.6bb25aad.chunk.js"
+  },
+  {
+    "revision": "c760080037f47b495d3c",
+    "url": "/junctions-io-web/static/css/main.bbaf466c.chunk.css"
+  },
+  {
+    "revision": "4a7bfa945ef1e6d617c6ba72368c6022",
+    "url": "/junctions-io-web/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.631c31b10600e46af3061623b650e1bc.js"
+  "/junctions-io-web/precache-manifest.a5c1d48ef70ea034d1be65d78e5b5210.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/junctions-io-web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
